e8aa81d500b4d352f54b3bda7f0b51619dabc2fc
victorcuervo <vcuervo@gmail.com>
2025-03-03 17:14:32 +0100

    Incluida creación de datos en el ejemplo

--- consultas/contar-elementos-distintos.sql
+++ consultas/contar-elementos-distintos.sql
@@ -7,4 +7,25 @@
  * @description Cuenta los resultados de una consulta, pero solo los que sean distintos
  */
 
-select count(distinct ideditorial) from libros
+-- create
+CREATE TABLE libros (
+    isbn VARCHAR(13) PRIMARY KEY,
+    titulo VARCHAR(100),
+    autor VARCHAR(100)
+);
+
+-- insert
+INSERT INTO libros (isbn, titulo, autor) VALUES ('978-1-4493-0814-8', 'What Is HTML5?', 'Brett McLaughlin');
+INSERT INTO libros (isbn, titulo, autor) VALUES ('970-26-0518-0', 'Cómo Programar en Java', 'Paul Deitel');
+INSERT INTO libros (isbn, titulo, autor) VALUES ('978-84-415-2348-7', 'Java SE 6', 'F. Javier Moldes');
+INSERT INTO libros (isbn, titulo, autor) VALUES ('978-1-4493-1439-2', 'What''s New in Java 7?', 'Madhusudhan Konda');
+INSERT INTO libros (isbn, titulo, autor) VALUES ('978-1-4493-1608-2', 'Just Spring', 'Madhusudhan Konda');
+INSERT INTO libros (isbn, titulo, autor) VALUES ('978-1-4493-2838-2', 'Just Spring Data Access', 'Madhusudhan Konda');
+
+
+-- fetch 
+-- 6 libros
+SELECT count(*) FROM libros;
+
+-- 4 autores
+SELECT count(distinct autor) FROM libros;
